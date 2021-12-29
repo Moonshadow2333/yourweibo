3b00e6be48ec0a726cc3350c68ce74e1eecf8ab7
moonshadow <1815265375@qq.com>
2021-12-29 02:47:02 +0000

    基础页面

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/help','StaticPagesController@help')->name('help');
