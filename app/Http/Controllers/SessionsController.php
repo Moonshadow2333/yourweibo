f9aecad9205d56192b7b0389bb72ad6ba28e8512
moonshadow <1815265375@qq.com>
2021-12-30 12:09:36 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -24,7 +24,7 @@ class SessionsController extends Controller
 
     }
     public function destroy(){
-        return view('');
+        return '退出成功';
     }
 
 }
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,6 +2,27 @@
         <div class="container">
             <a href="{{route('home')}}" class="navbar-brand">Yourweibo App</a>
             <ul class="navbar-nav justify-content-end">
+                @if(Auth::check())
+                <li class="nav-item">
+                    <a href="#" class="nav-link">用户列表</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">个人中心</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        编辑资料
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <form action="{{route('logout')}}" method="post">
+                        {{csrf_field()}}
+                        {{method_field('DELETE')}}
+                        <button class="btn btn-danger" type="submit">删除</button>
+                    </form>
+                </li>
+                @else
                 <li class="nav-item"><a class="nav-link" href="{{route('help')}}">
                     帮助
                 </a></li>
@@ -10,6 +31,7 @@
                         登录
                     </a>
                 </li>
+                @endif
             </ul>
         </div>
     </nav>
