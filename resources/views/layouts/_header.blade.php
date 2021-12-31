6212ef951832f767bcfc8ab810db51fdaa850f5b
moonshadow <1815265375@qq.com>
2021-12-31 08:20:19 +0000

    用户微博列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -8,7 +8,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a href="#" class="nav-link">个人中心</a>
+                    <a href="{{route('users.show',Auth::user())}}" class="nav-link">个人中心</a>
                 </li>
                 <li class="nav-item">
                     <a href="{{route('users.edit',Auth::user())}}" class="nav-link">
