5b2756ce245e6266e2552114654e06719a43a9da
moonshadow <1815265375@qq.com>
2021-12-31 06:50:05 +0000

    重置密码

--- resources/views/emails/reset_link.blade.php
+++ resources/views/emails/reset_link.blade.php
@@ -10,8 +10,8 @@
     <h1>您正在尝试找回密码</h1>
     <p>
         请点击以下链接进行下一步操作：
-        <a href="{{route('password.reset',$token)}}">
-            {{route('password.reset',$token)}}
+        <a href="{{ route('password.reset', $token) }}">
+              {{ route('password.reset', $token) }}
         </a>
     </p>
     <p>
